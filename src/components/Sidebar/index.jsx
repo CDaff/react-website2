f5820a478a35da98e7c7a2563d7eda16321376d2
CDaff <daffroncammeron@gmail.com>
2023-09-23 01:31:20 -0500

    Left Side Nav Bar Component Completed the left side nav bar with responsive design and links. 3 Main icons - Home, About, Contact Social Media icons - LinkedIn, Github, Youtube, Twitter
    Home Component
    Started working on the home page component.
    Began creating the general design of the page.

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,28 +1,57 @@
 import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
-import Logos from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
+import LogoS from '../../assets/images/logo-s.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faSkype, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => {
     return(
         <>
             <div className="nav-bar">
-                <Link className='logo' to='/'>
-                    <img src={Logos} alt="logo" />
-                    <img className='sub-logo' src={LogoSubtitle} alt="slobodan" />
+                <Link className="logo" to="/">
+                    <img src={LogoS} alt="logo" />
+                    <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
                 </Link>
                 <nav>
-                    <NavLink exact="true" activeclassname='active' to='/'>
+                    <NavLink exact="true" activeclassname="active" to="/">
                         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
                     </NavLink>
-                    <NavLink exact="true" activeclassname='active' className='about-link' to='/about'>
+                    <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
                         <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
                     </NavLink>
-                    <NavLink exact="true" activeclassname='active' className='contact-link' to='/contact'>
+                    <NavLink exact="true" activeclassname='active' className="contact-link" to="/contact">
                         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
                     </NavLink>
                 </nav>
+                <ul>
+                    <li>
+                        <a target="_blank" rel='noreferrer' href=''>
+                            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                        </a>
+                    </li>
+                    <li>
+                        <a target="_blank" rel="noreferrer" href="">
+                            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                        </a>
+                    </li>
+                    <li>
+                        <a target="_blank" rel="noreferrer" href="">
+                            <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+                        </a>
+                    </li>
+                    <li>
+                        <a target="_blank" rel="noreferrer" href="">
+                            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+                        </a>
+                    </li>
+                    {/*<li>
+                        <a target="_blank" rel="noreferrer" href="skype:live:[user_name]">
+                            <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
+                        </a>
+                    </li>*/}
+                </ul>
             </div>
        </> 
     )

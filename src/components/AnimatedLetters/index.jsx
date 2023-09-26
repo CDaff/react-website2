8a033dac9e87619fc60ea705f949e16e38bcfb19
CDaff <daffroncammeron@gmail.com>
2023-09-25 23:23:16 -0500

    HOME COMPONENT Fixed a bug in the HOME component; index.jsx in HOME had a bug which prevented the text from rendering so the page was blank and the letters wouldn't turn yellow while hovering over them. This bug was caused by return being called in the wrong location of the useEffect hook. It was called immediate before setTimeout instead of before setLetterClass.
    LAYOUT COMPONENT
    Fixed a styles best practices error in Layout index.jsx. The strings for className were '' instead of "".

--- src/components/AnimatedLetters/index.jsx
+++ src/components/AnimatedLetters/index.jsx
@@ -4,15 +4,13 @@ import './index.scss';
 const AnimatedLetters = ({ letterClass, strArray, index}) => {
     return(
         <span>
-            {
-                strArray.map((char, i) =>(
+            {strArray.map((char, i) => (
                   <span key={ char+i } className={`${letterClass} _${i+index}`}>
                     {char}
-                  </span>  
-                ))
-            }
+                  </span>
+                ))}
         </span>
-    );
+    )
 };
 
 export default AnimatedLetters;

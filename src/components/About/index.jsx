cf8df38c2722ecef234bd4abb9f3fc2fdb3a757c
CDaff <daffroncammeron@gmail.com>
2023-09-28 21:31:23 -0500

    Added Pacman loader animation to About and Home pages.

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -4,6 +4,7 @@ import AnimatedLetters from "../AnimatedLetters";
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from "@fortawesome/free-brands-svg-icons";
+import Loader from "react-loaders";
 
 const About = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
@@ -15,50 +16,53 @@ const About = () => {
       }, []);
     
     return(
-        <div className="container about-page">
-            <div className="text-zone">
-                <h1>
-                    <AnimatedLetters letterClass={letterClass} strArray={['A','b','o','u','t',' ','M','e']} index={15} />
-                </h1>
-                <p>
-                    I'm an ambitious front-end developer looking for a role in
-                    an established IT compnay with the opportuntity to work with the latest
-                    technologies on challenging and diverse projects.
-                </p>
-                <p>
-                    I'm quitely confident, naturally curious, and perpetually working on
-                    improving my abilities one problem at a time.
-                </p>
-                <p>
-                    If I need to define myself in one sentence it would be a dedicated
-                    person, with a strong desire to perform my best, always improve,
-                    always remain passionate, and obssess over the things I enjoy.
-                </p>
-            </div>
+        <>
+            <div className="container about-page">
+                <div className="text-zone">
+                    <h1>
+                        <AnimatedLetters letterClass={letterClass} strArray={['A','b','o','u','t',' ','M','e']} index={15} />
+                    </h1>
+                    <p>
+                        I'm an ambitious front-end developer looking for a role in
+                        an established IT compnay with the opportuntity to work with the latest
+                        technologies on challenging and diverse projects.
+                    </p>
+                    <p>
+                        I'm quitely confident, naturally curious, and perpetually working on
+                        improving my abilities one problem at a time.
+                    </p>
+                    <p>
+                        If I need to define myself in one sentence it would be a dedicated
+                        person, with a strong desire to perform my best, always improve,
+                        always remain passionate, and obssess over the things I enjoy.
+                    </p>
+                </div>
 
-            <div className="stage-cube-cont">
-                <div className="cubespinner">
-                    <div className="face1">
-                        <FontAwesomeIcon icon={faAngular} color="#DD0031" />
-                    </div>
-                    <div className="face2">
-                        <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-                    </div>
-                    <div className="face3">
-                        <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-                    </div>
-                    <div className="face4">
-                        <FontAwesomeIcon icon={faReact} color="#53D4F4" />
-                    </div>
-                    <div className="face5">
-                        <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-                    </div>
-                    <div className="face6">
-                        <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                <div className="stage-cube-cont">
+                    <div className="cubespinner">
+                        <div className="face1">
+                            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+                        </div>
+                        <div className="face2">
+                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                        </div>
+                        <div className="face3">
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        </div>
+                        <div className="face4">
+                            <FontAwesomeIcon icon={faReact} color="#53D4F4" />
+                        </div>
+                        <div className="face5">
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                        </div>
+                        <div className="face6">
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <Loader type="pacman" />
+        </>
     )
 };
 
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -4,6 +4,7 @@ import LogoTitle from '../../assets/images/logo-s.png';
 import AnimatedLetters from "../AnimatedLetters";
 import Logo from './Logo'
 import './index.scss';
+import Loader from "react-loaders";
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
@@ -37,6 +38,7 @@ const Home = () => {
                 </div>
                 <Logo />
             </div>
+            <Loader type="pacman" />
         </>
     )
 }

01303402844db4a4cc42cba6e921dca32a04aa19
CDaff <40277491+CDaff@users.noreply.github.com>
2023-10-06 14:59:58 -0500

    Began working on Portfolio Component

---
+++ src/components/Portfolio/index.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import './index.scss'
+import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters";
+
+const Portfolio = () => {
+    const [letterClass, setLetterClass] = useState('text-animate');
+    
+    return (
+        <>
+            <div className="container portfolio-page">
+                <h1 className="page-title">
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={"Portfolio".split("")}
+                        index={15} 
+                    />
+                </h1>
+            </div>
+            <Loader type="pacman" />
+        </>
+    )
+}
+
+export default Portfolio;
